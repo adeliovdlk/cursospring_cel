21087974eed24ea39878ca4062804ab7efccb458
Delios <adelioa.c@gmail.com>
2021-03-19 09:32:04 -0300

    Initial commit

---
+++ src/main/java/com/projetocel/curso/CursospringudemyApplication.java
@@ -0,0 +1,13 @@
+package com.projetocel.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringudemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetocel/curso/CursospringudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetocel.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
